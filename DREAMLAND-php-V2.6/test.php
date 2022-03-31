98591681b412a1f5640c47c489af229538afc9c3
kezh97 <kezh97@gmail.com>
2022-03-31 02:28:39 -0600

    Update test.php

--- DREAMLAND-php-V2.6/test.php
+++ DREAMLAND-php-V2.6/test.php
@@ -13,59 +13,10 @@ if (!$entrada) {
 }
 
 //REQUEST
-$consulta = "SELECT ID, name FROM propiedad";
+$consulta = "SELECT ID, name, location_name, price, rooms, baths, parking, description FROM property";
 $resultado = $entrada -> query($consulta);
 
-if ($resultado->num_rows > 0) {
-	while ($row = $resultado ->fetch_assoc()) {
-		echo "ID: " . $row["ID"]. " ".  "Name: " . $row["name"] . "<br>";	
-		echo '"<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
-					
-					<div class="p-3 item-card-inner" id="list-border">
-						
-						<div class="mt-1 my-1 card-img">
-							<img src="" alt="">
-						</div>
-
-						<div class="card-details">
-
-							<div class="location-name">
-								<h5>' . $name . ' </h5>
-								<p>' . $location_name . '</p>
-								<hr class="w-75 mx-auto">
-							</div>
-
-							<div class="row details">
-
-								<div class="col-4 detail-item" id="list-border">
-									<p>' . $rooms .'</p>
-								</div>
-
-								<div class="col-4 detail-item" id="list-border">
-									<p>' . $baths . '</p>
-								</div>
-
-								<div class="col-4 detail-item" id="list-border">
-									<p>'. $parking . '</p>
-								</div>
-							</div>	
-						</div>
-
-						<div class="card-description text-justify">
-							<p>' . $desc . '</p>
-						</div>
-
-						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
-					</div>
-				</div>"';
-	}
-	
-} else{
-	echo '<script> console.log("0 resultados.")</script>';
-};
-
-echo '
+?>
 <!-- LIST AREA -->
 	<section id="listing-property-section">
 		<div class="container py-5 text-center">
@@ -74,13 +25,54 @@ echo '
 				<hr>
 			</div>
 			
-			<div class="listing row text-center">';
+			<div class="listing row text-center">
+				<?php  
+				if ($resultado->num_rows > 0) {
 
-			
+					while ($row = $resultado ->fetch_assoc()) {
+						?>
+							<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
+								<div class="p-3 item-card-inner" id="list-border">
+									<div class="mt-1 my-1 card-img">
+										<img src="img/house7.webp" alt="">
+									</div>
 
-			'</div>
+									<div class="card-details">
+										<div class="location_name">
+											<h5><?php  echo $row["name"]; ?></h5>
+											<p><?php  echo $row["location_name"];?></p>
+											<hr class="w-75 mx-auto">
+										</div>
+										<div class="row details">
+											<div class="col-4 detail-item" id="list-border">
+												<p><?php echo $row["rooms"]; ?> Cuartos</p>
+											</div>
+
+											<div class="col-4 detail-item" id="list-border">
+												<p><?php echo $row["baths"]; ?> Baños</p>
+											</div>
+
+											<div class="col-4 detail-item" id="list-border">
+												<p><?php echo $row["parking"]; ?> Parqueos</p>
+											</div>
+										</div>	
+									</div>
+
+									<div class="card-description text-justify">
+										<p> <?php echo $row["description"]; ?></p>
+									</div>
+
+									<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
+								</div>
+							</div>
+						
+					<?php  
+					};
+				};
+				?>
+				</div>
 		</div>
-	</section>';
-
+	</section>
+<?php
 include 'footer.php';
 ?>
