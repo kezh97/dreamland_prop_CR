6360d43d91cdaf9e7c4b7f097ec9ccffa28632ef
kezh97 <kezh97@gmail.com>
2022-04-03 01:51:54 -0600

    Update home.php

--- DREAMLAND-php-V2.6/home.php
+++ DREAMLAND-php-V2.6/home.php
@@ -1,6 +1,4 @@
-<?php 
-
-echo 
+<?php echo 
 '<!-- CAROUSEL -->	
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 		<ol class="carousel-indicators">
@@ -12,21 +10,21 @@ echo
   		<div class="carousel-inner" id="carousel-home">
     		<div class="carousel-item active">
       			<img class="" src="img/city.webp" alt="First slide">
-      			<div class="carousel-caption">
+      			<div class="carousel-caption home-caption">
     				<h5>Dreamland Properties CR</h5>
     				<p>Your best choice to find your dreamhouse</p>
   				</div>
     		</div>
     		<div class="carousel-item">
       			<img class="" src="img/house7.webp" alt="Second slide">
-      			<div class="carousel-caption">
+      			<div class="carousel-caption home-caption">
     				<h5>Contact us!</h5>
     				<p>Meet our best realtor.</p>
   				</div>
     		</div>
     		<div class="carousel-item">
       			<img class="" src="img/tour.webp" alt="Third slide">
-      			<div class="carousel-caption" id="home-caption">
+      			<div class="carousel-caption home-caption">
     				<h5>Explore Costa Ricas natural paradise</h5>
     				<p>One with the nature</p>
   				</div>
@@ -52,19 +50,20 @@ echo
 	<!-- OBJETIVES -->	
 	<section class="container py-4">
 		<div class="row">
-			<div class="col-md text-center mx-2 pt-5 pb-2 services-show">
+
+			<div class="col-md text-center m-2 pt-5 pb-2 services-show">
 				<img loading="lazy" src="img/realtor.webp" alt="realtor" width="180" height="180">
 				<h3>Best Treatment</h3>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum accusantium expedita voluptate optio qui velit.</p>
 			</div>
 
-			<div class="col-md text-center mx-2 pt-5 pb-2 services-show">
+			<div class="col-md text-center m-2 pt-5 pb-2 services-show">
 				<img loading="lazy" src="img/realestate.webp" alt="realtor" width="180" height="180">
 				<h3>You ask it, we find it for you</h3>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum accusantium expedita voluptate optio qui velit.</p>
 			</div>
 
-			<div class="col-md text-center mx-2 pt-5 pb-2 services-show">
+			<div class="col-md text-center m-2 pt-5 pb-2 services-show">
 				<img loading="lazy" src="img/touricon.svg" alt="realtor" width="180" height="180">
 				<h3>Tour Services</h3>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum accusantium expedita voluptate optio qui velit.</p>
@@ -86,6 +85,179 @@ echo
 		<div class="mt-5 text-center">
 			<img loading="lazy" class="img-fluid" src="img/house1.webp" alt="house">
 		</div>
-	</section>';
-	include 'swiper.php'; 
+	</section>	
+
+	<!-- PROPERTY SECTION SLIDER -->
+	<section id="property-section">
+		<div class="prop-slider container">
+			<h6 class="h6 color-primary m-0">Recent</h6>
+			<h1 class="h1 h1-responsive mb-4">Recently Added Properties</h1>
+			<div class="text-right">
+				<a href="property-list.php">Find More Project <i class="fas fa-long-arrow-alt-right"></i></a>
+			</div>
+
+			<div id="property-slider">
+				<!-- Slider main container -->
+				<div class="swiper swiper-container">
+					<!-- Additional required wrapper -->
+					<div class="swiper-wrapper">
+						<!-- Slides -->
+						<div class="swiper-slide">
+							<div class="property-list">
+								<div class="image">
+									<img loading="lazy" src="img/house1.webp" alt="">
+								</div>
+								<div class="text-right">
+									<h4 class="h5">$15.000</h4>
+								</div>
+								<div class="d-flex justify-content-between mb-4">
+									<div class="item">
+										<h4 class="h5 m-0">Santa Ana City</h4>
+										<p class="m-0">San José</p>
+									</div>
+
+									<div class="item d-flex align-self-center">
+										<i class="fas fa-bed mr-2 align-self-center"></i>
+										<span class="mr-1">5</span>
+										<i class="fas fa-toilet mr-2 align-self-center"></i>
+										<span class="mr-1">2</span>
+									</div>
+								</div>
+
+								<button type="button" class="btn btn-primary">Preview</button>
+							</div>
+						</div>
+
+						<div class="swiper-slide">
+							<div class="property-list">
+								<div class="image">
+									<img loading="lazy" src="img/house7.webp" alt="">
+								</div>
+								<div class="text-right">
+									<h4 class="h5">$15.000</h4>
+								</div>
+								<div class="d-flex justify-content-between mb-4">
+									<div class="item">
+										<h4 class="h5 m-0">Santa Ana City</h4>
+										<p class="m-0">San José</p>
+									</div>
+
+									<div class="item d-flex align-self-center">
+										<i class="fas fa-bed mr-2 align-self-center"></i>
+										<span class="mr-1">5</span>
+										<i class="fas fa-toilet mr-2 align-self-center"></i>
+										<span class="mr-1">2</span>
+									</div>
+								</div>
+
+								<button type="button" class="btn btn-primary">Preview</button>
+							</div>
+						</div>
+						<div class="swiper-slide">
+							<div class="property-list">
+								<div class="image">
+									<img loading="lazy" src="img/house3.webp" alt="">
+								</div>
+								<div class="text-right">
+									<h4 class="h5">$15.000</h4>
+								</div>
+								<div class="d-flex justify-content-between mb-4">
+									<div class="item">
+										<h4 class="h5 m-0">Santa Ana City</h4>
+										<p class="m-0">San José</p>
+									</div>
+
+									<div class="item d-flex align-self-center">
+										<i class="fas fa-bed mr-2 align-self-center"></i>
+										<span class="mr-1">5</span>
+										<i class="fas fa-toilet mr-2 align-self-center"></i>
+										<span class="mr-1">2</span>
+									</div>
+								</div>
+
+								<button type="button" class="btn btn-primary">Preview</button>
+							</div>
+						</div>
+						<div class="swiper-slide">
+							<div class="property-list">
+								<div class="image">
+									<img loading="lazy" src="img/house4.webp" alt="">
+								</div>
+								<div class="text-right">
+									<h4 class="h5">$15.000</h4>
+								</div>
+								<div class="d-flex justify-content-between mb-4">
+									<div class="item">
+										<h4 class="h5 m-0">Santa Ana City</h4>
+										<p class="m-0">San José</p>
+									</div>
+
+									<div class="item d-flex align-self-center">
+										<i class="fas fa-bed mr-2 align-self-center"></i>
+										<span class="mr-1">5</span>
+										<i class="fas fa-toilet mr-2 align-self-center"></i>
+										<span class="mr-1">2</span>
+									</div>
+								</div>
+
+								<button type="button" class="btn btn-primary">Preview</button>
+							</div>
+						</div>
+
+						<div class="swiper-slide">
+							<div class="property-list">
+								<div class="image">
+									<img loading="lazy" src="img/house5.webp" alt="">
+								</div>
+								<div class="text-right">
+									<h4 class="h5">$15.000</h4>
+								</div>
+								<div class="d-flex justify-content-between mb-4">
+									<div class="item">
+										<h4 class="h5 m-0">Santa Ana City</h4>
+										<p class="m-0">San José</p>
+									</div>
+
+									<div class="item d-flex align-self-center">
+										<i class="fas fa-bed mr-2 align-self-center"></i>
+										<span class="mr-1">5</span>
+										<i class="fas fa-toilet mr-2 align-self-center"></i>
+										<span class="mr-1">2</span>
+									</div>
+								</div>
+
+								<button type="button" class="btn btn-primary">Preview</button>
+							</div>
+						</div>
+
+						<div class="swiper-slide">
+							<div class="property-list">
+								<div class="image">
+									<img loading="lazy" src="img/house6.webp" alt="">
+								</div>
+								<div class="text-right">
+									<h4 class="h5">$15.000</h4>
+								</div>
+								<div class="d-flex justify-content-between mb-4">
+									<div class="item">
+										<h4 class="h5 m-0">Santa Ana City</h4>
+										<p class="m-0">San José</p>
+									</div>
+
+									<div class="item d-flex align-self-center">
+										<i class="fas fa-bed mr-2 align-self-center"></i>
+										<span class="mr-1">5</span>
+										<i class="fas fa-toilet mr-2 align-self-center"></i>
+										<span class="mr-1">2</span>
+									</div>
+								</div>
+
+								<button type="button" class="btn btn-primary">Preview</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>' 
 ?>
