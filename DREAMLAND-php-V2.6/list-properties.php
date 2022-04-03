4557f093a353b7d42b6608964fa74b46699cd60c
kezh97 <kezh97@gmail.com>
2022-04-03 01:52:33 -0600

    Update list-properties.php

--- DREAMLAND-php-V2.6/list-properties.php
+++ DREAMLAND-php-V2.6/list-properties.php
@@ -10,13 +10,11 @@ echo '
 			</div>
 			
 			<div class="listing row text-center">
-
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
 					<div class="p-3 item-card-inner" id="list-border">
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house7.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -27,32 +25,25 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
-
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
 					<div class="p-3 item-card-inner" id="list-border">
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house2.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -63,32 +54,25 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
-
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
 					<div class="p-3 item-card-inner" id="list-border">
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house6.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -99,32 +83,25 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
-
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
 					<div class="p-3 item-card-inner" id="list-border">
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house4.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -135,32 +112,25 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
-
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
 					<div class="p-3 item-card-inner" id="list-border">
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house1.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -171,23 +141,18 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
@@ -195,7 +160,6 @@ echo '
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house3.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -206,23 +170,18 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
@@ -230,7 +189,6 @@ echo '
 						<div class="mt-1 my-1 card-img">
 							<img src="img/house5.webp" alt="">
 						</div>
-
 						<div class="card-details">
 							<div class="location-name">
 								<h5>Villas Alegre</h5>
@@ -241,35 +199,29 @@ echo '
 								<div class="col-4 detail-item" id="list-border">
 									<p>3 Cuartos</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Baños</p>
 								</div>
-
 								<div class="col-4 detail-item" id="list-border">
 									<p>2 Parqueos</p>
 								</div>
 							</div>	
 						</div>
-
 						<div class="card-description text-justify">
 							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam error, dignissimos</p>
 						</div>
-
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-
 					</div>
 				</div>
 				
 			</div>
 		</div>
 	</section>
-
-
 	<section id="property-modal">
 		<div class="modal fade" id="listing_modal" tabindex="-1" aria-labelledby="listing-name-label" aria-hidden="true">
-			  	<div class="modal-dialog modal-xl">
-			    	<div class="modal-content">
+
+		  	<div class="modal-dialog modal-xl">
+		    	<div class="modal-content">
 			      	<div class="modal-header">
 			        	<h4 class="modal-title" id="listing-name-label">Santa Ana, San José</h4>
 			        	<i class="fas fa-times" data-bs-dismiss="modal" aria-label="Close"></i>
@@ -283,19 +235,17 @@ echo '
 								    <li data-target="#carousel-ind" data-slide-to="1"></li>
 								    <li data-target="#carousel-ind" data-slide-to="2"></li>
 								  </ol>
-
 								  <div class="carousel-inner" >
 								    <div class="carousel-item list-carousel active">
 								      <img src="img/house1.webp" class="" alt="">
 								    </div>
-								    <div class="carousel-item">
-								      <img src="img/house2.webp" class="" alt="">
+								    <div class="carousel-item list-carousel">
+								      <img src="img/house7.webp" class="" alt="">
 								    </div>
-								    <div class="carousel-item">
+								    <div class="carousel-item list-carousel">
 								      <img src="img/house3.webp" class="" alt="">
 								    </div>
 								  </div>
-
 								  <a class="carousel-control-prev" href="#carousel-ind" role="button" data-slide="prev">
 								    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 								    <span class="sr-only">Previous</span>
@@ -323,19 +273,15 @@ echo '
 				        			<div class="col-6 text-center py-2 my-1" id="modal-overview-item">
 				        				<i class="fas fa-comments-dollar"></i> $3500000
 				        			</div>
-
 				        			<div class="col-6 text-center py-2 my-1" id="modal-overview-item">
 				        				<i class="fas fa-hashtag"></i> ID: DLCR-XXXXXX
 				        			</div>
-
 				        			<div class="col-4 text-center py-2 my-1" id="modal-overview-item">
 				        				<i class="fas fa-bed"></i> 3 Cuartos
 				        			</div>
-
 				        			<div class="col-4 text-center py-2 my-1" id="modal-overview-item">
 				        				<i class="fas fa-bath"></i> 2 Baños
 				        			</div>
-
 				        			<div class="col-4 text-center py-2 my-1" id="modal-overview-item">
 				        				<i class="fas fa-car"></i> 2 Parqueos
 				        			</div>
@@ -367,24 +313,21 @@ echo '
 			        			
 			        		</div>
 			        	</div>
-
 			        	<div class="modal-map pt-3">
 			        		<div class="container">
 			        			<h4 class="h4 color-primary m-0">Ubicación</h4>
 			        			<hr>
 			        			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15695.330747442886!2d-84.00539163079384!3d10.434848233776973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa099734387841d%3A0xa5d62bb729a851df!2sBarrio%20Flaminia%2C%20Heredia%2C%20Sarapiqu%C3%AD!5e0!3m2!1ses!2scr!4v1646257878309!5m2!1ses!2scr" width="100%" height="400px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 			        		</div>
-			        	</div>
-			        		
-			      	</div>
+			        	</div>	
+		      		</div>
 
 			      	<div class="modal-footer">
 			        	<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-			      	</div>
+			      	</div> 	
 			    </div>
 			</div>
 		</div>	
-
 	</section>';
 	include 'swiper.php';
 	include 'footer.php';
