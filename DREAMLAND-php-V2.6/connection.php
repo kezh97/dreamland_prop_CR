2260f53a7d3ed7949a8c7b33eadd0615deb285d8
kezh97 <kezh97@gmail.com>
2022-04-09 01:37:20 -0600

    Update connection.php

--- DREAMLAND-php-V2.6/connection.php
+++ DREAMLAND-php-V2.6/connection.php
@@ -1,11 +1,12 @@
 <?php 
 
-$user = "root@localhost";
+$user = "root";
 $pass = "";
 $server = "127.0.0.1";
 $db = "test";
 
-$conn = new mysqli($server, $user, $pass, $db);
+$conn = new mysqli($server, $user, $pass);
+$conn->select_db($db);
 if ($conn->connect_error) {
 	die('Error: (' . $conn->connect_errno .')' . $conn->connect_error);
 }
