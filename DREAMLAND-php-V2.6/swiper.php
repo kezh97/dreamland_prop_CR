2eec8be7a970f28fe43951b371db149897454b00
kezh97 <kezh97@gmail.com>
2022-04-09 21:27:02 -0600

    Update swiper.php

--- DREAMLAND-php-V2.6/swiper.php
+++ DREAMLAND-php-V2.6/swiper.php
@@ -35,12 +35,12 @@ $result_general = $conn -> query($query);
 								<h4 class="h5">DLCR-<?php echo $row["id"]; ?>: <?php echo $row["name"] .', '. $row["location_name"]; ?></h4>
 							</div>
 							<div class="d-flex justify-content-between mb-4">
-								<div class="item px-1">
+								<div class="item py-3">
 									<h4 class="h5 m-0 location-name"></h4>
 									<p class="m-0 location-name">$<?php echo $row["price"]; ?></p>
 								</div>
 
-								<div class="item d-flex align-self-center">
+								<div class="item d-flex align-self-center py-3">
 									<i class="fas fa-bed mr-2 align-self-center"></i>
 									<span class="mr-4"><?php echo $row["rooms"]; ?></span>
 									<i class="fas fa-toilet mr-2 align-self-center"></i>
