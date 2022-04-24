3f2fdaab0839ab8c7f4e0b1c7e2706c9eca67b64
kezh97 <kezh97@gmail.com>
2022-04-24 03:40:02 -0600

    Update outer-card.php

--- DREAMLAND-php-V2.6/outer-card.php
+++ DREAMLAND-php-V2.6/outer-card.php
@@ -1,8 +1,8 @@
 <?php  
 
 //REQUEST
-$query = "SELECT property_id, name, location_name, price, rooms, baths, parking, description FROM property";
-$result_general = $conn -> query($query);
+$query_join = "SELECT s1.property_id, s1.name, s1.location_name, s1.rooms, s1.baths, s1.description, s1.price, s1.parking, s2.img_path FROM `property` AS s1 JOIN img_repository AS s2 on s1.property_id = s2.property_id WHERE img_repo_id = 1";
+$result_general = $conn -> query($query_join);
 
 if (mysqli_num_rows($result_general) > 0) {
 	while ($row =  mysqli_fetch_array($result_general)) {
@@ -10,7 +10,7 @@ if (mysqli_num_rows($result_general) > 0) {
 		<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
 			<div class="p-3 item-card-inner" id="list-border">
 				<div class="mt-1 my-1 card-img">
-					<img src="img/house7.webp" alt="">
+					<img src="<?php  echo $row["img_path"];?>" alt="">
 				</div>
 
 				<div class="card-details">
