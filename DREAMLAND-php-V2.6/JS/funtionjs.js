0475d91d5980c14c1f3677e321f3d3239e7648c6
kezh97 <kezh97@gmail.com>
2022-04-25 17:28:58 -0600

    Update funtionjs.js

--- DREAMLAND-php-V2.6/JS/funtionjs.js
+++ DREAMLAND-php-V2.6/JS/funtionjs.js
@@ -1,45 +1,57 @@
-$(document).ready(function(){
-       		$('.boton').click(function(){
-       			var received_id = $(this).data('id');
-       			$.ajax({
-       				url:'ajax-listing-prop.php',
-       				type: 'post',
-       				data: {prop_id: received_id},
-       				success: function(response){
-       					$('.modal-content').html(response);
-       					$('#listing_modal').modal('show');
-       				}
-       			});
-
-       		});
-       	});
 
 // LANGUAGE TOGGLE SWITCHER
-		$('.btn-toggle').click(function() {
-    		$(this).find('.btn').toggleClass('active');  
-    
-    		if ($(this).find('.btn-primary').length>0) {
-    			$(this).find('.btn').toggleClass('btn-primary');
-    		}
-    
-    		$(this).find('.btn').toggleClass('btn-default');
-       	});
+$('.btn-toggle').click(function() {
+	$(this).find('.btn').toggleClass('active');  
 
-		// SWIPER SLIDER
-       	var mySwiper = new Swiper('.swiper-container', {
-  			// Optional parameters
-  			loop: true,
-  			freeMode: true,
-  			centeredSlides: true,
-  			breakpoints: {
-  				299: {
-  					slidesPerView: 1,
-  					spaceBetween: 40
-  				},
+	if ($(this).find('.btn-primary').length>0) {
+		$(this).find('.btn').toggleClass('btn-primary');
+	}
 
-  				999: {
-  					slidesPerView: 3,
-  					spaceBetween: 30
-  				}
-  			}
+	$(this).find('.btn').toggleClass('btn-default');
+	});
+
+// SWIPER SLIDER
+	var mySwiper = new Swiper('.swiper-container', {
+		// Optional parameters
+		loop: true,
+		freeMode: true,
+		centeredSlides: true,
+		breakpoints: {
+			299: {
+				slidesPerView: 1,
+				spaceBetween: 40
+			},
+
+			999: {
+				slidesPerView: 3,
+				spaceBetween: 30
+			}
+		}
+});
+
+// NAV SELECTOR
+jQuery(function($){
+    $('.menu ul li a').filter(function() {
+    var locationUrl = window.location.href;
+    var currentItemUrl = $(this).prop('href');
+
+    return currentItemUrl === locationUrl;
+    }).parent('li').addClass('active');
+});
+
+// MODAL ID SELECTOR
+$(document).ready(function(){
+	$('.boton').click(function(){
+		var received_id = $(this).data('id');
+		$.ajax({
+			url:'ajax-listing-prop.php',
+			type: 'post',
+			data: {prop_id: received_id},
+			success: function(response){
+				$('.modal-content').html(response);
+				$('#listing_modal').modal('show');
+			}
 		});
+
+	});
+});
