c6eaac77e472404f3e7f945d84853c7da2d4fcb5
kezh97 <kezh97@gmail.com>
2022-06-26 02:19:14 -0600

    Update ajax-listing-prop.php

--- DREAMLAND-php-V2.6/ajax-listing-prop.php
+++ DREAMLAND-php-V2.6/ajax-listing-prop.php
@@ -2,7 +2,6 @@
 include 'connection.php';
 
 $id = $_POST['prop_id'];
-$dir = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15695.330747442886!2d-84.00539163079384!3d10.434848233776973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa099734387841d%3A0xa5d62bb729a851df!2sBarrio%20Flaminia%2C%20Heredia%2C%20Sarapiqu%C3%AD!5e0!3m2!1ses!2scr!4v1646257878309!5m2!1ses!2scr';
 $query_join = "SELECT s1.property_id, s1.name, s1.location_name, s1.rooms, s1.baths, s1.description, s1.price, s1.parking, s1.google_maps_location, s2.img_path FROM `property` AS s1 JOIN img_repository AS s2 on s1.property_id = s2.property_id WHERE s1.property_id =".$id;
 $result_query_by_id = mysqli_query($conn, $query_join);
 
@@ -68,12 +67,14 @@ while ($row = mysqli_fetch_array($result_query_by_id)) {
 				<h4 class="h4 ">Overview</h4>
 				<hr>
 				<div class="">
+						<div class="text-center py-2 my-1" id="modal-overview-item">
+		    				<i class="fas fa-hashtag"></i> ID: DLCR-<?php  echo $row["property_id"]; ?>
+		    			</div>
+
 		    			<div class="text-center py-2 my-1" id="modal-overview-item">
 		    				<i class="fas fa-comments-dollar"></i> $<?php  echo $row["price"]; ?>
 		    			</div>
-		    			<div class="text-center py-2 my-1" id="modal-overview-item">
-		    				<i class="fas fa-hashtag"></i> ID: DLCR-<?php  echo $row["property_id"]; ?>
-		    			</div>
+		    			
 		    			<div class="text-center py-2 my-1" id="modal-overview-item">
 		    				<i class="fas fa-bed"></i> <?php  echo $row["rooms"]; ?> Cuartos
 		    			</div>
@@ -85,37 +86,6 @@ while ($row = mysqli_fetch_array($result_query_by_id)) {
 		    			</div>
 				</div>
 			
-			</div>
-		</div>
-
-		<div class="modal-description text-center pt-3">
-			<div class="container">
-				<h3>Descripción principal</h3>
-				<p class="text-justify">Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Id eaque dolorum ipsum aspernatur, asperiores dolores officia nisi cupiditate, voluptatibus excepturi quia ut velit. Sint libero labore ipsam neque incidunt ab. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint repellat corrupti, ex nesciunt omnis impedit commodi quibusdam sunt laborum, vitae voluptates. Sed voluptas, aut dolorem repellat deserunt! Ea, modi maiores?</p>
-			</div>
-		</div>
-
-		<div class="modal-overview">
-			<div class="container-fluid">
-				<h4 class="h4 ">Overview</h4>
-				<hr>
-				<div class="row">
-		    			<div class="col-6 text-center py-2 my-1" id="modal-overview-item">
-		    				<i class="fas fa-comments-dollar"></i> $<?php  echo $row["price"]; ?>
-		    			</div>
-		    			<div class="col-6 text-center py-2 my-1" id="modal-overview-item">
-		    				<i class="fas fa-hashtag"></i> ID: DLCR-<?php  echo $row["property_id"]; ?>
-		    			</div>
-		    			<div class="col-4 text-center py-2 my-1" id="modal-overview-item">
-		    				<i class="fas fa-bed"></i> <?php  echo $row["rooms"]; ?> Cuartos
-		    			</div>
-		    			<div class="col-4 text-center py-2 my-1" id="modal-overview-item">
-		    				<i class="fas fa-bath"></i> <?php  echo $row["baths"]; ?> Baños
-		    			</div>
-		    			<div class="col-4 text-center py-2 my-1" id="modal-overview-item">
-		    				<i class="fas fa-car"></i> <?php  echo $row["parking"]; ?> Parqueos
-		    			</div>
-				</div>
 			</div>
 		</div>
 
@@ -141,6 +111,14 @@ while ($row = mysqli_fetch_array($result_query_by_id)) {
 				</div>
 				</div>
 				
+			</div>
+		</div>
+
+		<div class="modal-multimedia">
+			<div class="container">
+				<h4 class="h4 color-primary m-0">Video Recorrido</h4>
+				<hr>
+				<iframe width="100%" height="600px" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</div>
 		</div>
 
