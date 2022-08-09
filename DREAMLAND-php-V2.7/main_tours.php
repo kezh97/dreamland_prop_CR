453a6b720c84f9cc9609d04c4a3feb67b7eb2a0b
kezh97 <kezh97@gmail.com>
2022-08-08 19:08:24 -0600

    Update main_tours.php

--- DREAMLAND-php-V2.7/main_tours.php
+++ DREAMLAND-php-V2.7/main_tours.php
@@ -21,51 +21,7 @@ include 'header.php';
 		</div>
 
 		<div class="row tours-packs-list">
-
-			<div class="outer-tours-packs-card col-lg-4 col-md-6 p-2">
-				<div class="tours-packs-card card">
-					<img src="img/nature1.webp" class="card-img-top" alt="...">
-	  				<div class="card-body">
-	    				<h5 class="card-title">Tour Pack: 1</h5>
-	    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    				<button type="button" data-id="1" class="boton_tour btn btn-primary" data-bs-toggle="modal" data-bs-target="#tour_modal">Show More</button>	
-	  				</div>
-	  			</div>
-			</div>
-
-			<div class="outer-tours-packs-card col-lg-4 col-md-6 p-2">
-				<div class="tours-packs-card card">
-					<img src="img/nature2.webp" class="card-img-top" alt="...">
-	  				<div class="card-body">
-	    				<h5 class="card-title">Tour Pack: 2</h5>
-	    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    				<button type="button" data-id="<?php echo $row['tour_id']; ?>" class="boton_tour btn btn-primary" data-bs-toggle="modal" data-bs-target="#tour_modal">Show More</button>	
-	  				</div>
-	  			</div>
-			</div>
-
-			<div class="outer-tours-packs-card col-lg-4 col-md-6 p-2">
-				<div class="tours-packs-card card">
-					<img src="img/nature3.webp" class="card-img-top" alt="...">
-	  				<div class="card-body">
-	    				<h5 class="card-title">Tour Pack: 3</h5>
-	    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    				<button type="button" data-id="<?php echo $row['tour_id']; ?>" class="boton_tour btn btn-primary" data-bs-toggle="modal" data-bs-target="#tour_modal">Show More</button>	
-	  				</div>
-	  			</div>
-			</div>
-
-			<div class="outer-tours-packs-card col-lg-4 col-md-6 p-2">
-				<div class="tours-packs-card card">
-					<img src="img/nature4.webp" class="card-img-top" alt="...">
-	  				<div class="card-body">
-	    				<h5 class="card-title">Tour Pack: 4</h5>
-	    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    				<button type="button" data-id="<?php echo $row['tour_id']; ?>" class="boton_tour btn btn-primary" data-bs-toggle="modal" data-bs-target="#tour_modal">Show More</button>	
-	  				</div>
-	  			</div>
-			</div>
-
+			<?php  include 'comp_tours_list.php'; ?>
 		</div>
 	</div>
 </section>
