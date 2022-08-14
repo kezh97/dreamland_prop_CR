b12ac3401e99436ca1ebb4dec98999ae894280e9
kezh97 <kezh97@gmail.com>
2022-08-14 17:01:39 -0600

    Update comp_prop_list.php

--- DREAMLAND-php-V2.7/comp_prop_list.php
+++ DREAMLAND-php-V2.7/comp_prop_list.php
@@ -8,41 +8,34 @@ $result_general = $conn -> query($query_join);
 if (mysqli_num_rows($result_general) > 0) {
 	while ($row =  mysqli_fetch_array($result_general)) {
 ?>
-		<div class="col-lg-4 col-md-6 col-sm-12 p-1 item-card-outer">
-			<div class="p-3 item-card-inner" id="list-border">
-				<div class="mt-1 my-1 card-img">
-					<img src="<?php  echo $row["img_path"];?>" alt="">
-				</div>
-
-				<div class="card-details">
-					<div class="location-name pt-3">
-						<h5>DLCR-<?php echo $row["property_id"] .': '. $row["name"]; ?> </h5>
-						<p><?php  echo $row["location_name"];?></p>
-						<hr class="w-75 mx-auto">
-					</div>
-					<div class="row details">
-						<div class="col-4 detail-item" id="list-border">
-							<p><i class="fas fa-bed"></i> <?php echo $row["rooms"]; ?></p>
+		<div class="outer-global-card col-lg-4 col-md-6 p-2">
+				<div class="global-card card">
+					<img src="<?php  echo $row["img_path"];?>" class="card-img-top" alt="...">
+	  				<div class="card-body">
+	    				<div class="location-name pt-3">
+							<h4><strong>DLCR-<?php echo $row["property_id"] .': '. $row["name"]; ?> </strong></h4>
+							<p><?php  echo $row["location_name"];?></p>
+							<hr class="w-75 mx-auto">
 						</div>
 
-						<div class="col-4 detail-item" id="list-border">
-							<p><i class="fas fa-bath"></i> <?php echo $row["baths"]; ?></p>
-						</div>
+	    				<div class="row details">
+	    					<div class="col-4 detail-item">
+								<p><i class="fas fa-bed"></i> <?php echo $row["rooms"]; ?></p>
+							</div>
 
-						<div class="col-4 detail-item" id="list-border">
-							<p><i class="fas fa-car"></i> <?php echo $row["parking"]; ?></p>
-						</div>
-					</div>	
-				</div>
+							<div class="col-4 detail-item">
+								<p><i class="fas fa-bath"></i> <?php echo $row["baths"]; ?></p>
+							</div>
 
-				<div class="card-description text-justify">
-					<p> <?php echo $row["description"]; ?></p>
-				</div>
-				
-				<button type="button" data-id="<?php echo $row['property_id']; ?>" class="boton_prop btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>
-				
+							<div class="col-4 detail-item">
+								<p><i class="fas fa-car"></i> <?php echo $row["parking"]; ?></p>
+							</div>
+	    				</div>
+	    				<p class="card-text"><?php echo $row['description'] ?></p>
+	    				<button type="button" data-id="<?php echo $row['property_id'] ?>" class="boton_prop btn btn-primary" data-bs-toggle="modal" data-bs-target="#listing_modal">Show More</button>	
+	  				</div>
+	  			</div>
 			</div>
-		</div>
 <?php  
 		};
 	};
